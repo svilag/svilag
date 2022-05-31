874e15b4914519b6977124dc44de9b71e0a0eb64
svilag <svilag@umich.edu>
2022-05-31 16:28:23 -0400

    center nav in header

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    var menu = document.getElementById("navLinks");
+    if (menu.style.display != "none") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
+    }
+}
+
+
